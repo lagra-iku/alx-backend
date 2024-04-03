8e4c67d0e20242d08e11255d50447083d2fc8a95
Grace O. Nweke <lagra2001@gmail.com>
2024-04-03 22:35:45 +0000

    solving for task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,7 +3,6 @@
 LRU caching
 """
 
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -11,7 +10,6 @@ class LRUCache(BaseCaching):
     """
     a class LRUCache that inherits from BaseCaching and is a caching system:
     """
-
     def __init__(self):
         """
         initialization
@@ -23,27 +21,26 @@ class LRUCache(BaseCaching):
         """
         Must assign to the dictionary self.cache_data the item value
         """
-        if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                lru_key = self.access_order.pop(0)
-                del self.cache_data[lru_key]
-                print("DISCARD:", lru_key)
-            else:
-                if key in self.cache_data:
-                    self.access_order.remove(key)
-                self.access_order.append(key)
+        if key is not None and item is not None:
             self.cache_data[key] = item
+            if key not in self.access_order:
+                self.access_order.append(key)
+            else:
+                self.access_order.append(
+                    self.access_order.pop(self.access_order.index(key)))
+            if len(self.access_order) > BaseCaching.MAX_ITEMS:
+                discard = self.access_order.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """
         return the value in self.cache_data linked to key.
         """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        else:
+        if key is not None:
             if key in self.cache_data:
+                # Update access order
                 self.access_order.remove(key)
                 self.access_order.append(key)
                 return self.cache_data[key]
+        return None
