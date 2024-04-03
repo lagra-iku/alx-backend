8aa3fe6a3a3e3e223e02fc78d1576f5b0844a725
Grace O. Nweke <lagra2001@gmail.com>
2024-04-03 21:49:50 +0000

    solving for task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,14 +4,25 @@ Basic Dictionary
 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """ 
+    """
     a class BasicCache that inherits from BaseCaching and is a caching system
     """
+    def __init__(self):
+        """
+        initialization
+        """
+        super().__init__()
+
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
+        else:
+            pass
 
     def get(self, key):
         if key is not None:
             return self.cache_data.get(key)
+        else:
+            return None
