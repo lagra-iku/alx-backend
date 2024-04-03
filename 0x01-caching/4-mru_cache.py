ce78e8f0fb0a1a69b7454931c2bec98d22fad1d4
Grace O. Nweke <lagra2001@gmail.com>
2024-04-03 22:44:18 +0000

    solving for task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+MRU caching
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    a class MRUCache that inherits from BaseCaching and is a caching system:
+    """
+
+    def __init__(self):
+        """
+        initialization
+        """
+        super().__init__()
+        self.access_order = []
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data the item value
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.access_order:
+                self.access_order.append(key)
+            else:
+                self.access_order.append(
+                    self.access_order.pop(self.access_order.index(key)))
+            if len(self.access_order) > BaseCaching.MAX_ITEMS:
+                discard = self.access_order.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        else:
+            item = self.cache_data.pop(key)
+            self.cache_data[key] = item
+            return item
