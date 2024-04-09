7c29c7b32698dae005a83816f1c1f425030234a0
Grace O. Nweke <lagra2001@gmail.com>
2024-04-10 00:06:12 +0100

    solving for task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,9 +3,13 @@
 """Basic Flask app, with a single route"""
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
