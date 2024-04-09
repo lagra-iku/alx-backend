f94d3e9873dcc66ba570e373b5437c8d7b6e69f4
Grace O. Nweke <lagra2001@gmail.com>
2024-04-10 00:18:54 +0100

    solving for task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
 """Basic Flask app, with a single route"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route("/")
