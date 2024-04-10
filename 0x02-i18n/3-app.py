22c57e3b8ff60726ece4adb2a1e4d4acf5d99de3
Grace O. Nweke <lagra2001@gmail.com>
2024-04-10 01:53:44 +0100

    solving for task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -15,18 +15,14 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
-babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale():
-    """
-    if a user is logged in, use the locale from the user settings
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
